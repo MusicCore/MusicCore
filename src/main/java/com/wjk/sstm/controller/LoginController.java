987782478f53ba90696f9f433ce9e25f564b8c60
wjk <your email>
2018-12-28 18:12:41 +0800

    taken格式修改+新增密码修改接口和登出接口-补充

--- src/main/java/com/wjk/sstm/controller/LoginController.java
+++ src/main/java/com/wjk/sstm/controller/LoginController.java
@@ -11,9 +11,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
+@RestController
 @RequestMapping(value = "/api")
 public class LoginController {
 
